fcc9e9718692c6bfa05dc846f6d42e5ea40b72c2
goat1967 <fran0532@gmail.com>
2021-02-11 22:29:36 -0500

    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class HelloTheWorld {
+public class HelloWorld {
     public static void main(String[] args){
 System.out.println("Hello World!");
     }
